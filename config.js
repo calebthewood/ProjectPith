081c150ccb93c25dc7061329f20aceb748f64229
calebthewood <calebwood.cs@gmail.com>
2022-10-04 15:49:10 -0700

    Add notion sdk

---
+++ config.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/** Shared config for application; can be required many places. */
+
+require("dotenv").config();
+require("colors");
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+
+const PORT = +process.env.PORT || 3001;
+
+// Use dev database, testing database, or via env var, production database
+function getDatabaseUri() {
+  return (process.env.NODE_ENV === "test")
+      ? "pith_test"
+      : process.env.DATABASE_URL || "pith";
+}
+
+// Speed up bcrypt during tests, since the algorithm safety isn't being tested
+//
+// WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+
+const NOTION_TOKEN = process.env.NOTION_TOKEN || null;
+const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID || null;
+
+console.log("Pith Config:".green);
+console.log("SECRET_KEY:".yellow, SECRET_KEY);
+console.log("PORT:".yellow, PORT.toString());
+console.log("BCRYPT_WORK_FACTOR:".yellow, BCRYPT_WORK_FACTOR);
+console.log("Notion Database:".yellow, NOTION_DATABASE_ID && "My Notebook");
+// console.log("Database:".yellow, getDatabaseUri());
+console.log("---");
+
+module.exports = {
+  SECRET_KEY,
+  PORT,
+  BCRYPT_WORK_FACTOR,
+  getDatabaseUri,
+  NOTION_TOKEN,
+  NOTION_DATABASE_ID,
+};
