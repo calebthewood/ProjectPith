0537ec073e6a8d69fec086a35a3acf619af1610d
calebthewood <calebwood.cs@gmail.com>
2023-06-08 23:31:05 -0700

    removes message responder api

--- twilioAPI.js
+++ twilioAPI.js
@@ -2,9 +2,6 @@
 
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE, MY_PHONE } = require("./config");
 
-
-// const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
 function sendSms(message) {
   const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
   return client.messages
@@ -15,14 +12,6 @@ function sendSms(message) {
     })
     .then(message => console.log(message.sid));
 }
-
-// client.messages
-//   .create({
-//     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-//     from: TWILIO_PHONE,
-//     to: MY_PHONE
-//   })
-//   .then(message => console.log(message.sid));
 
 module.exports = {
   sendSms
