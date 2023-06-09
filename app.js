fdc7722ce89161622dc2ae953a9370e2f3dbbdcb
calebthewood <calebwood.cs@gmail.com>
2023-06-08 19:46:10 -0700

    Adds chronos router import

--- app.js
+++ app.js
@@ -4,20 +4,27 @@
 
 const express = require("express");
 const cors = require("cors");
+
 const { NotFoundError } = require("./expressError");
+
 const blogRoutes = require("./routes/blog");
 const tweetsRoutes = require("./routes/tweets");
 const metricsRoutes = require("./routes/metrics");
+const chronosRoutes = require("./routes/chronos");
+
 const morgan = require("morgan");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 
 app.use("/blog", blogRoutes);
 app.use("/tweets", tweetsRoutes);
 app.use("/metrics", metricsRoutes);
+app.use("/chronos", chronosRoutes);
 
 /** Handle 404 errors -- this matches everything */
 app.use(function (req, res, next) {
